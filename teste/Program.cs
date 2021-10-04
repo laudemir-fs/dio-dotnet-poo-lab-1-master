162e7c69642eeb3cdf5708f04730b6daa4f14236
laude <laude@DESKTOP-DLNAIC6>
2021-10-04 08:59:07 -0300

    Adicionar arquivos de projeto.

---
+++ teste/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            double pi = 3.14159, raio;
+
+                raio = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("A=" + (pi * (raio * raio)).ToString("0,0000"));
+
+                Console.ReadKey();
+        }
+    }
+}
